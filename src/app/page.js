bacd6b7cf27736017f68407d6dedc580a8928454
Julie <julieeriksen09@hotmail.com>
2024-05-02 13:05:36 +0200

    Test commit

--- src/app/page.js
+++ src/app/page.js
@@ -23,7 +23,7 @@ export default function Home() {
       <div>
         <a href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer">
           <h2>
-            Docs <span>-&gt;</span>
+            Yes <span>-&gt;</span>
           </h2>
           <p>Find in-depth information about Next.js features and API.</p>
         </a>

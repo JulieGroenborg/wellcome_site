f35b1736b914e0f6ea6a479506024ba1b03bfb3a
Julie <julieeriksen09@hotmail.com>
2024-05-09 08:57:06 +0200

    api information

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,8 @@
 import HeaderOne from "./components/global/HeaderOne";
 
+//API url: https://hovludcpqudqvcqteblj.supabase.co/rest/v1/Hold
+//API key: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhvdmx1ZGNwcXVkcXZjcXRlYmxqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQwMzI4MzEsImV4cCI6MjAyOTYwODgzMX0.5K-wz_oerFZ5hmvUq0IOPgJHn0e1sRYh57y_8pFqnKk
+
 export default function Home() {
   return (
     <>

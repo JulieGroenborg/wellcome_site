1384dcc148905f5cd4c7b50647010b2d4e0e974a
Julie <julieeriksen09@hotmail.com>
2024-05-10 10:47:48 +0200

    Lavet CategoryCard og opstart på kategori-page

--- src/app/components/kategoriside/CategoryCard.jsx
+++ src/app/components/kategoriside/CategoryCard.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function CategoryCard({ href, title, category }) {
   return (
-    <Link href={href} prefetch={false}>
+    <Link className={styles.link} href={href} prefetch={false}>
       <section className={`${styles.card} ${styles[category]}`}>
         <div className={styles.overlay}>
           <h2 className={styles.title}>{title}</h2>
@@ -11,14 +11,4 @@ export default function CategoryCard({ href, title, category }) {
       </section>
     </Link>
   );
-}
-
-// import styles from "./HeaderOne.module.scss";
-
-// export default function HeaderOne({ title, color }) {
-//   return <h1 className={`${styles[color]}`}>{title}</h1>;
-// }
-
-{
-  /* <section className={`${styles.card} ${styles[kategory]}`}></section> */
 }
--- src/app/kategorier/page.jsx
+++ src/app/kategorier/page.jsx
@@ -1,16 +1,24 @@
+import "./kategori.scss";
 import CategoryCard from "../components/kategoriside/CategoryCard";
 
 export default function kategorier() {
   return (
     <>
       <main>
-        <CategoryCard category="indoor_cycling" title="Wellcome indoor cycling" href="noget"></CategoryCard>
-        <CategoryCard category="energy" title="Wellcome energy" href="noget"></CategoryCard>
-        <CategoryCard category="functional" title="Wellcome functional" href="noget"></CategoryCard>
-        <CategoryCard category="heat" title="Wellcome heat" href="noget"></CategoryCard>
-        <CategoryCard category="nordic_strong" title="Wellcome nordic strong" href="noget"></CategoryCard>
-        <CategoryCard category="pleasure" title="Wellcome pleasure" href="noget"></CategoryCard>
-        <CategoryCard category="reformer" title="Wellcome reformer" href="noget"></CategoryCard>
+        <section className="card_container">
+          <div className="flex_cards">
+            <CategoryCard category="indoor_cycling" title="Wellcome indoor cycling" href="noget"></CategoryCard>
+            <CategoryCard category="energy" title="Wellcome energy" href="noget"></CategoryCard>
+            <CategoryCard category="functional" title="Wellcome functional" href="noget"></CategoryCard>
+            <CategoryCard category="heat" title="Wellcome heat" href="noget"></CategoryCard>
+          </div>
+          <div className="flex_cards">
+            <CategoryCard category="nordic_strong" title="Wellcome nordic strong" href="noget"></CategoryCard>
+            <CategoryCard category="pleasure" title="Wellcome pleasure" href="noget"></CategoryCard>
+            <CategoryCard category="body_mind" title="Wellcome body mind" href="noget"></CategoryCard>
+            <CategoryCard category="reformer" title="Wellcome reformer" href="noget"></CategoryCard>
+          </div>
+        </section>
       </main>
     </>
   );

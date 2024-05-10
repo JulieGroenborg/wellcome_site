bd678eff20db8350a0a34ef3bda890301bbdbcac
Freja Fischer <frej657e@stud.kea.dk>
2024-05-10 09:50:20 +0200

    Fixed classname mistake in linkbtn

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,22 +1,23 @@
 import HeaderOne from "./components/global/HeaderOne";
+import LinkBtn from "./components/global/LinkBtn";
 import ClassItem from "./components/holdplan/ClassItem";
 
 //API url: https://hovludcpqudqvcqteblj.supabase.co/rest/v1/Hold
 //API key: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhvdmx1ZGNwcXVkcXZjcXRlYmxqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQwMzI4MzEsImV4cCI6MjAyOTYwODgzMX0.5K-wz_oerFZ5hmvUq0IOPgJHn0e1sRYh57y_8pFqnKk
 
-let headersList = {
-  apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhvdmx1ZGNwcXVkcXZjcXRlYmxqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQwMzI4MzEsImV4cCI6MjAyOTYwODgzMX0.5K-wz_oerFZ5hmvUq0IOPgJHn0e1sRYh57y_8pFqnKk",
-  Prefer: "return=representation",
-};
+// let headersList = {
+//   apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhvdmx1ZGNwcXVkcXZjcXRlYmxqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQwMzI4MzEsImV4cCI6MjAyOTYwODgzMX0.5K-wz_oerFZ5hmvUq0IOPgJHn0e1sRYh57y_8pFqnKk",
+//   Prefer: "return=representation",
+// };
 
-let response = await fetch("https://hovludcpqudqvcqteblj.supabase.co/rest/v1/Hold", {
-  method: "GET",
-  headers: headersList,
-});
+// let response = await fetch("https://hovludcpqudqvcqteblj.supabase.co/rest/v1/Hold", {
+//   method: "GET",
+//   headers: headersList,
+// });
 
-let data = await response.json();
+// let data = await response.json();
 
-console.log(data);
+// console.log(data);
 
 export default function Home() {
   return (

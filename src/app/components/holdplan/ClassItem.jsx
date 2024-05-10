2c2012017f336c9f1b1d01202dd5b36c4b5d0a2b
Freja Fischer <frej657e@stud.kea.dk>
2024-05-10 14:24:29 +0200

    testing if sticky works in safari?

--- src/app/components/holdplan/ClassItem.jsx
+++ src/app/components/holdplan/ClassItem.jsx
@@ -1,15 +1,23 @@
+import LinkBtn from "../global/LinkBtn";
 import styles from "./ClassItem.module.scss";
 
-export default function ClassItem({ classtitle }) {
+export default function ClassItem({ time, classtitle, coach, location, chosenClassItem, setChosenClassItem }) {
   return (
-    <section className={styles.container}>
-      <input type="radio" name="weekdays" id={classtitle} value={classtitle} className={styles.input} />
-      <label htmlFor={classtitle} className={styles.label}>
-        <span>tid</span>
-        <span>{classtitle}</span>
-        <span>træner</span>
-        <span>lokale</span>
-      </label>
-    </section>
+    <>
+      <section className={styles.container} onFocus={() => setChosenClassItem(classtitle)}>
+        <input type="radio" name="weekdays" id={classtitle} value={classtitle} className={styles.input} />
+        <label htmlFor={classtitle} className={styles.label}>
+          <span>{time}</span>
+          <span>{classtitle}</span>
+          <span className={styles.grey_text}>{coach}</span>
+          <span className={styles.grey_text}>{location}</span>
+        </label>
+      </section>
+      {/* {chosenClassItem === classtitle && (
+        <section className={styles.mobile_btn_section}>
+          <LinkBtn href="/" text="Tilmeld" variant="primary" />
+        </section>
+      )} */}
+    </>
   );
 }
--- src/app/holdplan/page.jsx
+++ src/app/holdplan/page.jsx
@@ -1,7 +1,8 @@
 "use client";
 import { useState } from "react";
 import styles from "./holdplan.module.scss";
-import DayRadio from "@/app/components/holdplan/DayRadio";
+import LinkBtn from "../components/global/LinkBtn";
+import ClassItem from "../components/holdplan/ClassItem";
 
 //API url: https://hovludcpqudqvcqteblj.supabase.co/rest/v1/Hold
 //API key: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhvdmx1ZGNwcXVkcXZjcXRlYmxqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQwMzI4MzEsImV4cCI6MjAyOTYwODgzMX0.5K-wz_oerFZ5hmvUq0IOPgJHn0e1sRYh57y_8pFqnKk
@@ -22,11 +23,14 @@ import DayRadio from "@/app/components/holdplan/DayRadio";
 
 export default function Holdplan() {
   const [chosenDay, setChosenDay] = useState(); //State til at vide hvilken dag er valgt til senere brug i koden ;-)
-  console.log(chosenDay);
+  const [chosenClassItem, setChosenClassItem] = useState("");
+
+  console.log("this is the chosen", chosenClassItem);
+
   return (
     <>
-      <main>
-        <section className={styles.day_section}>
+      <main className={styles.main}>
+        {/* <section className={styles.day_section}>
           <DayRadio day="mandag" setChosen={setChosenDay} />
           <DayRadio day="tirsdag" setChosen={setChosenDay} />
           <DayRadio day="onsdag" setChosen={setChosenDay} />
@@ -34,7 +38,26 @@ export default function Holdplan() {
           <DayRadio day="fredag" setChosen={setChosenDay} />
           <DayRadio day="lørdag" setChosen={setChosenDay} />
           <DayRadio day="søndag" setChosen={setChosenDay} />
-        </section>
+        </section> */}
+        <article className={styles.class_overview}>
+          <article>
+            <ClassItem classtitle="Nordic Strong Power" coach="Michael Andersen" time="13:45-11:00" location="CrossTraining" chosenClassItem={chosenClassItem} setChosenClassItem={setChosenClassItem} />
+            <ClassItem classtitle="Mindfull stræk og afspænding" coach="Frederik Tønder-Prien" time="13:45-11:00" location="CrossTraining" chosenClassItem={chosenClassItem} setChosenClassItem={setChosenClassItem} />
+            <ClassItem classtitle="Boldmassage af bindevævet" coach="Christina Præstkær" time="13:45-11:00" location="CrossTraining" chosenClassItem={chosenClassItem} setChosenClassItem={setChosenClassItem} />
+            <ClassItem classtitle="Nordic Strong Powe" coach="Michael Andersen" time="13:45-11:00" location="CrossTraining" chosenClassItem={chosenClassItem} setChosenClassItem={setChosenClassItem} />
+            <ClassItem classtitle="Mindfull stræk og afspændin" coach="Frederik Tønder-Prien" time="13:45-11:00" location="CrossTraining" chosenClassItem={chosenClassItem} setChosenClassItem={setChosenClassItem} />
+            <ClassItem classtitle="Boldmassage af bindevævt" coach="Christina Præstkær" time="13:45-11:00" location="CrossTraining" chosenClassItem={chosenClassItem} setChosenClassItem={setChosenClassItem} />
+            <ClassItem classtitle="Nordic Strong Pwer" coach="Michael Andersen" time="13:45-11:00" location="CrossTraining" chosenClassItem={chosenClassItem} setChosenClassItem={setChosenClassItem} />
+            <ClassItem classtitle="Mindfull stræk o afspænding" coach="Frederik Tønder-Prien" time="13:45-11:00" location="CrossTraining" chosenClassItem={chosenClassItem} setChosenClassItem={setChosenClassItem} />
+            <ClassItem classtitle="Boldmassae af bindevævet" coach="Christina Præstkær" time="13:45-11:00" location="CrossTraining" chosenClassItem={chosenClassItem} setChosenClassItem={setChosenClassItem} />
+          </article>
+          {chosenClassItem !== "" && (
+            <section className={styles.mobile_btn_section}>
+              <LinkBtn href="/" text="Tilmeld" variant="primary" />
+            </section>
+          )}
+        </article>
+        <footer className={styles.footer}>hello</footer>
       </main>
     </>
   );

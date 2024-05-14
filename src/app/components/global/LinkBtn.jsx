60ebc1bac18bfed501cedbfb5202281c0b8a68db
Freja Fischer <frej657e@stud.kea.dk>
2024-05-14 12:15:24 +0200

    tilføjet focus variabel + sat ny attribut til LinkBtn

--- src/app/components/global/LinkBtn.jsx
+++ src/app/components/global/LinkBtn.jsx
@@ -4,9 +4,12 @@ import styles from "@/app/components/global/LinkBtn.module.scss";
 //LinkBtn have five variants:
 //primary, secondary, tertiary_dark, tertiary_light and spa
 
-export default function LinkBtn({ href, text, variant }) {
+//Hvis komponentet skal have margin-top, så skal der sættes attribut margin=true
+
+export default function LinkBtn({ href, text, variant, margin }) {
+  const marginStyling = margin ? "margin" : "";
   return (
-    <Link href={href} prefetch={false} className={`${styles.wellcome_btn} ${styles[variant]}`}>
+    <Link href={href} prefetch={false} className={`${styles.wellcome_btn} ${styles[variant]} ${styles[marginStyling]}`}>
       {text}
     </Link>
   );

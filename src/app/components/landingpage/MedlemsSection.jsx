1f658a4b5c03ce5f387976180e763e48b762e6ba
SiffKL <Siff0407@stud.kea.dk>
2024-05-21 16:56:27 +0200

    Mdl. Cards fortsat

--- src/app/components/landingpage/MedlemsSection.jsx
+++ src/app/components/landingpage/MedlemsSection.jsx
@@ -1,10 +1,11 @@
 import styles from "./MedlemsSection.module.scss";
 import MedlemsCards from "./MedlemsCards";
 
-export default function MedlemsSection() {
+export default function MedlemsSection(title) {
   return (
     <section className={styles.full_section}>
       <h2>Vores Medlemskaber</h2>
+      {/* <h2 className={styles.title}>{title}</h2> */}
       <div className={styles.medlemscards_section}>
         <MedlemsCards text="Fitness Medlem"></MedlemsCards>
         <MedlemsCards text="Club Medlem"></MedlemsCards>

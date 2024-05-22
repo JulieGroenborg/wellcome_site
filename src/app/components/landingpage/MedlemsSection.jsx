14ae0b06d42995ad9e55f5b92932dded033fcf09
SiffKL <Siff0407@stud.kea.dk>
2024-05-22 13:54:17 +0200

    pull request rettelser done

--- src/app/components/landingpage/MedlemsSection.jsx
+++ src/app/components/landingpage/MedlemsSection.jsx
@@ -5,23 +5,14 @@ export default function MedlemsSection(title) {
   return (
     <section className={styles.full_section}>
       <h2>Vores medlemskaber</h2>
-      {/* <h2 className={styles.title}>{title}</h2> */}
+
       <div className={styles.medlemscards_section}>
         <MedlemsCards
           text="Fitness Medlem"
           image="medlems_image1"
-          // overlay="overlay"
         ></MedlemsCards>
-        <MedlemsCards
-          text="Club Medlem"
-          image="medlems_image2"
-          // overlay="overlay"
-        ></MedlemsCards>
-        <MedlemsCards
-          text="Young Medlem"
-          image="medlems_image3"
-          // overlay="overlay"
-        ></MedlemsCards>
+        <MedlemsCards text="Club Medlem" image="medlems_image2"></MedlemsCards>
+        <MedlemsCards text="Young Medlem" image="medlems_image3"></MedlemsCards>
       </div>
     </section>
   );

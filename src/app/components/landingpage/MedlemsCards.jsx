123f112fcc9759a162b2890dd7fe2ec13ba47e07
SiffKL <Siff0407@stud.kea.dk>
2024-05-22 13:43:43 +0200

    % overlay

--- src/app/components/landingpage/MedlemsCards.jsx
+++ src/app/components/landingpage/MedlemsCards.jsx
@@ -4,9 +4,7 @@ import styles from "./MedlemsCards.module.scss";
 export default function MedlemsCards({ text, href, image }) {
   return (
     <Link href={"/"} prefetch={false} className={`${styles.medlems_Link} `}>
-      <div
-        className={`${styles.medlems_card} ${styles[image]} ${styles.overlay}`}
-      >
+      <div className={`${styles.medlems_card} ${styles[image]}`}>
         <div className={styles.overlay}>{text}</div>
       </div>{" "}
     </Link>

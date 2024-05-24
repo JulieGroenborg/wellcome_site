6b7e0661a932c47b0ef703c64940fa04f78df12c
SiffKL <Siff0407@stud.kea.dk>
2024-05-24 09:56:45 +0200

    done deal info, hero

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -116,8 +116,6 @@ export default function Home() {
             </div>
             <div className={styles.box2}>
               <svg
-                width="80"
-                height="80"
                 viewBox="0 0 80 80"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
@@ -131,8 +129,8 @@ export default function Home() {
             </div>
             <div className={styles.box3}>
               <svg
-                width="80"
-                height="80"
+                // width="80"
+                // height="80"
                 viewBox="0 0 80 80"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
@@ -146,8 +144,8 @@ export default function Home() {
             </div>
             <div className={styles.box4}>
               <svg
-                width="80"
-                height="80"
+                // width="80"
+                // height="80"
                 viewBox="0 0 80 80"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"

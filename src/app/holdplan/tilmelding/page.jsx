e6d0252552b537e962c79b3118d7789ea38f7f10
Freja Fischer <frej657e@stud.kea.dk>
2024-05-27 12:52:51 +0200

    tilføje Layout komponent til ekstra side tilmelding

--- src/app/holdplan/tilmelding/page.jsx
+++ src/app/holdplan/tilmelding/page.jsx
@@ -1,16 +1,19 @@
 import HeaderOne from "@/app/components/global/HeaderOne";
+import Layout from "@/app/components/global/Layout";
 import Link from "next/link";
 import styles from "./tilmelding.module.scss";
 
 export default function Tilmeldning() {
   return (
     <>
-      <main className={styles.main}>
-        <HeaderOne title="Tilmeldt hold" color="black" />
-        <Link className={styles.link} href="/holdplan">
-          Tilbage til Holdplan
-        </Link>
-      </main>
+      <Layout current={{ nav: "", site: "" }}>
+        <main className={styles.main}>
+          <HeaderOne title="Tilmeldt hold" color="black" />
+          <Link className={styles.link} href="/holdplan">
+            Tilbage til Holdplan
+          </Link>
+        </main>
+      </Layout>
     </>
   );
 }

cc7888aa3ff6b025a6738641011ba771300b1416
Freja Fischer <frej657e@stud.kea.dk>
2024-05-28 15:45:18 +0200

    rettet medlemscard til at være h3

--- src/app/components/landingpage/MedlemsCards.jsx
+++ src/app/components/landingpage/MedlemsCards.jsx
@@ -5,7 +5,9 @@ export default function MedlemsCards({ text, href, image }) {
   return (
     <Link href={"/"} prefetch={false} className={`${styles.medlems_Link} `}>
       <section className={`${styles.medlems_card} ${styles[image]}`}>
-        <div className={styles.overlay}>{text}</div>
+        <div className={styles.overlay}>
+          <h3>{text}</h3>
+        </div>
       </section>{" "}
     </Link>
   );
